c79bc67e196e7cbd4930d66a8d271cf3ae9854a7
crickett <crickettcode@gmail.com>
2018-04-26 18:47:46 -0400

    schema completed

--- db/schema.js
+++ db/schema.js
@@ -4,4 +4,13 @@ const Schema = mongoose.Schema
 
 const CreatureSchema = new Schema({
 
+    name: String,
+    description: String
+
 })
+
+const Creature = mongoose.model('Creature', CreatureSchema)
+
+module.exports = {
+    Creature
+}
