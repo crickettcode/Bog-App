c0dba1125ebc006610e3e10c1e1b9adb2bd580a7
crickett <crickettcode@gmail.com>
2018-04-26 18:33:48 -0400

    looking at my project II

--- db/seeds.js
+++ db/seeds.js
@@ -6,6 +6,21 @@ const { Creature } = require('./schema')
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI)
 
+mongoose.connection.once('open', () => {
+    console.log('Whoop Mongo')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.error(`
+    What is going on Mongo?!
+    ${error}
+    `)
+    process.exit(-1)
+
+})
+
+
+
 const db = mongoose.connection
 const saved = async () => {
 
